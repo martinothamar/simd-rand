8c0db8509f8cc35111f756effe5689bf7029a8d9
Martin Othamar <martin@othamar.net>
2023-07-11 20:38:00 +0200

    simple distribution test

--- src/bin/profile.rs
+++ src/bin/profile.rs
@@ -1,10 +1,15 @@
-use rand_core::SeedableRng;
+use rand_core::{SeedableRng};
 use shishua::Shishua;
 
 fn main() {
-    let mut rng = Shishua::seed_from_u64(0);
+    let mut rng1 = Shishua::seed_from_u64(0x0DDB1A5E5BAD5EEDu64);
+    let mut rng2 = Shishua::seed_from_u64(0x0DDB1A5E5BAD5EEDu64);
 
-    for _ in 0..100_000_000 {
-        println!("{}", rng.next_f64())
+    for _ in 0..10 {
+        let value1 = rng1.next_f32();
+        let value2 = rng2.next_f64();
+        println!("{:.6}", value1);
+        println!("{:.6}", value2);
+        println!("--------");
     }
 }
