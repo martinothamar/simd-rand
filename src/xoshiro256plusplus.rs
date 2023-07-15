6eb57ca39434b965ed740380c28e9e166c1f7edf
Martin Othamar <martin@othamar.net>
2023-07-15 18:41:08 +0200

    hmm

--- src/xoshiro256plusplus.rs
+++ src/xoshiro256plusplus.rs
@@ -1,62 +1,56 @@
 use std::{arch::x86_64::*, mem::{transmute, self}};
 
-use rand_core::{RngCore, impls::fill_bytes_via_next, Error, SeedableRng, le::read_u64_into};
+use rand_core::{SeedableRng, le::read_u64_into};
 
-pub struct Xoshiro256PlusPlus {
-    s: [u64; 4],
-}
+pub struct Xoshiro256PlusPlusx4Seed(pub [u8; 128]);
 
 pub struct Xoshiro256PlusPlusx4 {
-    rng: [Xoshiro256PlusPlus; 4],
+    states: [[u64; 4]; 4],
+}
+impl Default for Xoshiro256PlusPlusx4Seed {
+    fn default() -> Xoshiro256PlusPlusx4Seed {
+        Xoshiro256PlusPlusx4Seed([0; 128])
+    }
 }
 
-impl SeedableRng for Xoshiro256PlusPlus {
-    type Seed = [u8; 32];
-
-    /// Create a new `Xoshiro256PlusPlus`.  If `seed` is entirely 0, it will be
-    /// mapped to a different seed.
-    #[inline]
-    fn from_seed(seed: [u8; 32]) -> Xoshiro256PlusPlus {
-        if seed.iter().all(|&x| x == 0) {
-            return Self::seed_from_u64(0);
-        }
-        let mut state = [0; 4];
-        read_u64_into(&seed, &mut state);
-        Xoshiro256PlusPlus { s: state }
+impl AsMut<[u8]> for Xoshiro256PlusPlusx4Seed {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
     }
+}
 
-    /// Create a new `Xoshiro256PlusPlus` from a `u64` seed.
-    ///
-    /// This uses the SplitMix64 generator internally.
-    fn seed_from_u64(mut state: u64) -> Self {
-        const PHI: u64 = 0x9e3779b97f4a7c15;
-        let mut seed = Self::Seed::default();
-        for chunk in seed.as_mut().chunks_mut(8) {
-            state = state.wrapping_add(PHI);
-            let mut z = state;
-            z = (z ^ (z >> 30)).wrapping_mul(0xbf58476d1ce4e5b9);
-            z = (z ^ (z >> 27)).wrapping_mul(0x94d049bb133111eb);
-            z = z ^ (z >> 31);
-            chunk.copy_from_slice(&z.to_le_bytes());
+impl SeedableRng for Xoshiro256PlusPlusx4 {
+    type Seed = Xoshiro256PlusPlusx4Seed;
+
+    fn from_seed(seed: Self::Seed) -> Self {
+        const SIZE: usize = mem::size_of::<u64>();
+        const LEN: usize = 4;
+        let mut states: [[u64; LEN]; LEN] = [[0; 4]; 4];
+        read_u64_into(&seed.0[..(SIZE * LEN * 1)], &mut states[0][..]);
+        read_u64_into(&seed.0[(SIZE * LEN * 1)..(SIZE * LEN * 2)], &mut states[1][..]);
+        read_u64_into(&seed.0[(SIZE * LEN * 2)..(SIZE * LEN * 3)], &mut states[2][..]);
+        read_u64_into(&seed.0[(SIZE * LEN * 3)..], &mut states[3][..]);
+
+        Self {
+            states
         }
-        Self::from_seed(seed)
     }
 }
 
 impl Xoshiro256PlusPlusx4 {
-    pub fn next_vecs(&mut self) -> __m256i {
+    pub fn next_m256i(&mut self) -> __m256i {
         unsafe {
             let s0 = _mm256_set_epi64x(
-                transmute::<_, i64>(self.rng[0].s[0]),
-                transmute::<_, i64>(self.rng[1].s[0]),
-                transmute::<_, i64>(self.rng[2].s[0]),
-                transmute::<_, i64>(self.rng[3].s[0])
+                transmute::<_, i64>(self.states[0][0]),
+                transmute::<_, i64>(self.states[1][0]),
+                transmute::<_, i64>(self.states[2][0]),
+                transmute::<_, i64>(self.states[3][0])
             );
             let s3 = _mm256_set_epi64x(
-                transmute::<_, i64>(self.rng[0].s[3]),
-                transmute::<_, i64>(self.rng[1].s[3]),
-                transmute::<_, i64>(self.rng[2].s[3]),
-                transmute::<_, i64>(self.rng[3].s[3])
+                transmute::<_, i64>(self.states[0][3]),
+                transmute::<_, i64>(self.states[1][3]),
+                transmute::<_, i64>(self.states[2][3]),
+                transmute::<_, i64>(self.states[3][3])
             );
 
             // s[0] + s[3]
@@ -71,6 +65,26 @@ impl Xoshiro256PlusPlusx4 {
             // rotl(...) + s[0]
             let result = _mm256_add_epi64(rotl, s0);
 
+            //         let t = self.s[1] << 17;
+            let s1 = _mm256_set_epi64x(
+                transmute::<_, i64>(self.states[0][1]),
+                transmute::<_, i64>(self.states[1][1]),
+                transmute::<_, i64>(self.states[2][1]),
+                transmute::<_, i64>(self.states[3][1])
+            );
+            let t = _mm256_sll_epi64(s1, _mm_set1_epi32(17));
+
+            //         self.s[2] ^= self.s[0];
+            //         self.s[3] ^= self.s[1];
+            //         self.s[1] ^= self.s[2];
+            //         self.s[0] ^= self.s[3];
+
+            //         self.s[2] ^= t;
+
+            //         self.s[3] = self.s[3].rotate_left(45);
+
+            //         result_plusplus
+
             result
         }
     }
@@ -78,7 +92,7 @@ impl Xoshiro256PlusPlusx4 {
     pub fn next_u64s(&mut self, mem: &mut U64x4) {
         assert!(mem::align_of_val(mem) % 32 == 0, "mem needs to be aligned to 32 bytes");
 
-        let vec = self.next_vecs();
+        let vec = self.next_m256i();
         unsafe {   
             _mm256_store_si256(transmute::<_, *mut __m256i>(&mut mem.0), vec);
         }
@@ -88,46 +102,46 @@ impl Xoshiro256PlusPlusx4 {
 #[repr(align(32))]
 pub struct U64x4([u64; 4]);
 
-impl RngCore for Xoshiro256PlusPlus {
-    #[inline]
-    fn next_u64(&mut self) -> u64 {
-        let result_plusplus = self.s[0]
-            .wrapping_add(self.s[3])
-            .rotate_left(23)
-            .wrapping_add(self.s[0]);
+// impl RngCore for Xoshiro256PlusPlus {
+//     #[inline]
+//     fn next_u64(&mut self) -> u64 {
+//         let result_plusplus = self.s[0]
+//             .wrapping_add(self.s[3])
+//             .rotate_left(23)
+//             .wrapping_add(self.s[0]);
 
-        let t = self.s[1] << 17;
+//         let t = self.s[1] << 17;
 
-        self.s[2] ^= self.s[0];
-        self.s[3] ^= self.s[1];
-        self.s[1] ^= self.s[2];
-        self.s[0] ^= self.s[3];
+//         self.s[2] ^= self.s[0];
+//         self.s[3] ^= self.s[1];
+//         self.s[1] ^= self.s[2];
+//         self.s[0] ^= self.s[3];
 
-        self.s[2] ^= t;
+//         self.s[2] ^= t;
 
-        self.s[3] = self.s[3].rotate_left(45);
+//         self.s[3] = self.s[3].rotate_left(45);
 
-        result_plusplus
-    }
+//         result_plusplus
+//     }
 
-    #[inline]
-    fn next_u32(&mut self) -> u32 {
-        // The lowest bits have some linear dependencies, so we use the
-        // upper bits instead.
-        (self.next_u64() >> 32) as u32
-    }
+//     #[inline]
+//     fn next_u32(&mut self) -> u32 {
+//         // The lowest bits have some linear dependencies, so we use the
+//         // upper bits instead.
+//         (self.next_u64() >> 32) as u32
+//     }
 
-    #[inline]
-    fn fill_bytes(&mut self, dest: &mut [u8]) {
-        fill_bytes_via_next(self, dest);
-    }
+//     #[inline]
+//     fn fill_bytes(&mut self, dest: &mut [u8]) {
+//         fill_bytes_via_next(self, dest);
+//     }
 
-    #[inline]
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
-        self.fill_bytes(dest);
-        Ok(())
-    }
-}
+//     #[inline]
+//     fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+//         self.fill_bytes(dest);
+//         Ok(())
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -149,16 +163,10 @@ mod tests {
     fn rng() {
         let mut seeder = SmallRng::seed_from_u64(0);
 
-        let rngs: [Xoshiro256PlusPlus; 4] = [
-            Xoshiro256PlusPlus::seed_from_u64(seeder.next_u64()),
-            Xoshiro256PlusPlus::seed_from_u64(seeder.next_u64()),
-            Xoshiro256PlusPlus::seed_from_u64(seeder.next_u64()),
-            Xoshiro256PlusPlus::seed_from_u64(seeder.next_u64()),
-        ];
+        let mut seed: Xoshiro256PlusPlusx4Seed = Default::default();
+        seeder.fill_bytes(&mut seed.0[..]);
 
-        let mut rng = Xoshiro256PlusPlusx4 {
-            rng: rngs,
-        };
+        let mut rng = Xoshiro256PlusPlusx4::from_seed(seed);
 
         let mut values = U64x4([0; 4]);
         rng.next_u64s(&mut values);
