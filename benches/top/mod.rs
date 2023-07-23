a9c87ee7a76f02b9f4aea180359387c24901a907
Martin Othamar <martin@othamar.net>
2023-07-23 12:03:19 +0200

    benchmark again rand simd feature

--- benches/top/mod.rs
+++ benches/top/mod.rs
@@ -1,16 +1,25 @@
 use std::{mem, arch::x86_64::*};
 
 use criterion::{measurement::Measurement, Criterion, Throughput, black_box, BenchmarkId};
+use rand::Rng;
 use rand_core::{RngCore, SeedableRng};
 use rand_xoshiro::Xoshiro256Plus;
 use simd_rand::specific::avx512::{U64x8, SimdRand, Xoshiro256PlusX8};
+use packed_simd_2::u64x8 as ps_u64x8;
 
 #[inline(always)]
-fn execute_original<RNG: RngCore>(rng: &mut RNG, data: &mut U64x8, i: usize) {
+fn execute_rand<RNG: RngCore>(rng: &mut RNG, data: &mut U64x8, i: usize) {
     for _ in 0..i {
         for i in 0..8 {
             black_box(&mut *data)[i] = rng.next_u64();
         }
+    }
+}
+
+#[inline(always)]
+fn execute_rand_vectorized<RNG: RngCore>(rng: &mut RNG, data: &mut ps_u64x8, i: usize) {
+    for _ in 0..i {
+        *black_box(&mut *data) = rng.gen::<ps_u64x8>();
     }
 }
 
@@ -29,12 +38,20 @@ pub fn add_top_benchmark<M: Measurement, const ITERATIONS: usize>(c: &mut Criter
     for iterations in iterations {
         group.throughput(Throughput::Bytes((iterations * mem::size_of::<__m512i>()) as u64));
         
-        let name = BenchmarkId::new(format!("Original/Xoshiro256+"), iterations);
+        let name = BenchmarkId::new(format!("Rand/Xoshiro256+"), iterations);
         group.bench_with_input(name, &iterations, |b, i| {
             let mut rng = Xoshiro256Plus::seed_from_u64(0x0DDB1A5E5BAD5EEDu64);
-            let mut data: U64x8 = Default::default();
+            let mut data = Default::default();
 
-            b.iter(|| execute_original(&mut rng, black_box(&mut data), black_box(*i)))
+            b.iter(|| execute_rand(&mut rng, black_box(&mut data), black_box(*i)))
+        });
+
+        let name = BenchmarkId::new(format!("RandVectorized/Xoshiro256+"), iterations);
+        group.bench_with_input(name, &iterations, |b, i| {
+            let mut rng = Xoshiro256Plus::seed_from_u64(0x0DDB1A5E5BAD5EEDu64);
+            let mut data = Default::default();
+
+            b.iter(|| execute_rand_vectorized(&mut rng, black_box(&mut data), black_box(*i)))
         });
         
         let name = BenchmarkId::new(format!("AVX512/Xoshiro256+"), iterations);
